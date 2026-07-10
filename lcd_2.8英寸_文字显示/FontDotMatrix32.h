4d9d3c950702fd37a636f370b6a0af3285099223
不负韶华 <1062835425@qq.com>
2026-07-10 17:19:15 +0800

    lcd-lvgl文字显示

---
+++ lcd_2.8英寸_文字显示/FontDotMatrix32.h
@@ -0,0 +1,11 @@
+﻿#ifndef __FONTDOTMATRIX32_H__
+#define __FONTDOTMATRIX32_H__
+
+/* 生成的字体点阵数据 扫描方式：逐行扫描  取模走向：低位在前 */
+
+extern const char* g_font_dot_matrix_32_index[12];
+
+extern const char g_font_dot_matrix_32[12][128];
+
+#endif
+
---
+++ lcd_2.8英寸_文字显示/lcd_2.8英寸_文字显示/USER/main.c
@@ -0,0 +1,94 @@
+#include "stm32f4xx.h"
+#include "delay.h"
+#include "usart.h"
+#include "lcd.h"
+#include "key.h"  
+#include "touch.h" 
+
+
+volatile u8 g_rx_flag = 0;
+volatile u8 g_count = 0;
+
+volatile u8 g_buffer[32] = {0};  
+volatile u8 g_rxuffer[32] = {0};
+
+
+
+
+void USART1_IRQHandler(void)
+{
+
+	//判断标志位是否置1
+	if(USART_GetITStatus(USART1, USART_IT_RXNE) == SET)
+	{
+		//清空中断标志位
+		USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+
+		//串口1接收数据
+		g_buffer[g_count++] = USART_ReceiveData(USART1);
+		
+		if(g_buffer[g_count - 1] == ':') //判断结束符是否为:
+		{
+			//过滤结束符   HCL11:   HCL10:
+			for(int i=0; i<g_count - 1; i++)
+			{
+				g_rxuffer[i] = g_buffer[i];
+			}
+			
+			g_rx_flag = 1; //接收数据标志位
+			
+			g_count = 0;//新的数据帧从g_buffer[0]开始接受
+			
+			
+			memset(g_buffer, 0, sizeof(g_buffer));
+		
+		
+		}
+		       
+	
+	}
+
+}
+
+
+
+
+int main(void)
+{
+
+	
+	u16 id;
+	//中断优先级分组，且一个工程只能设置一次
+	//抢占优先级范围:0~3  响应优先级:0~3
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	Delay_init();
+
+	Usart1_init(115200);
+
+
+ 	LCD_Init();					//LCD初始化 
+
+	TP_Init();					//触摸屏初始化
+	
+	LCD_Display_Dir(0);			//横屏，跟官方代码对不上
+	
+	LCD_Scan_Dir(U2D_R2L);		//从上到下，从右到左
+	
+	
+	// 重设屏幕的宽度与高度
+	lcddev.width=320;
+	lcddev.height=240;
+	
+	// 显示字符串
+	LCD_Clear(BLACK);
+	lcd_show_string(50,100,"不负韶华",YELLOW,BLACK,32);
+	
+	while(1)
+	{
+
+
+	}
+	
+	return 0;
+}
+
